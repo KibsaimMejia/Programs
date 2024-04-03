4bbf9daea6d485e0e9a539022f9c73a1e4d683db
Kibsaim Mejia <158346959+KibsaimMejia@users.noreply.github.com>
2024-04-03 04:10:36 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,14 @@
-# Debian/Ubuntu Linux Setup Steps
+# Windows/WSL/Ubuntu Linux Setup
 
 # Update and upgrade the system
 sudo apt-get update && sudo apt-get upgrade -y
+sudo apt update && \
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common && \
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - && \
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" && \
+sudo apt update && \
+sudo apt install -y docker-ce docker-compose && \
+sudo systemctl start docker && sudo systemctl enable docker && \
+sudo usermod -aG docker $USER && \
+sudo apt install -y nginx && \
+sudo systemctl start nginx && sudo systemctl enable nginx
